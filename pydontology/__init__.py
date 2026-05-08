377af0c26163b2562a64864f7adc5aa56e5ce6c6
Brandur K. Jacobsen <brandurjacobsen@gmail.com>
2026-05-08 22:37:38 +0200

    Update __init__.py

--- pydontology/__init__.py
+++ pydontology/__init__.py
@@ -9,15 +9,6 @@ from .pydontology import Pydontology
 from .rdfs import RDFSAnnotation
 from .settings import Settings
 from .shacl import SHACLAnnotation
-from .validators import (
-    val_datatype,
-    val_no_whitespace,
-    val_node_kind,
-    val_non_negative_int,
-    val_positive_int,
-    val_regex_pattern,
-    val_severity_cls,
-)
 
 __all__ = [
     "BaseContext",
@@ -29,11 +20,4 @@ __all__ = [
     "OWLAnnotation",
     "SHACLAnnotation",
     "Settings",
-    "val_datatype",
-    "val_no_whitespace",
-    "val_node_kind",
-    "val_non_negative_int",
-    "val_positive_int",
-    "val_regex_pattern",
-    "val_severity_cls",
 ]

2ba164b3ed9502dfd8e4eff96a27a7a305707c20
Brandur K. Jacobsen <brandurjacobsen@gmail.com>
2026-09-03 14:58:19 +0200

    Create bcp47.py

---
+++ pydontology/bcp47.py
@@ -0,0 +1,92 @@
+
+# List of BCP47 language tags with and without region specifiers.
+# Lifted from: https://gist.github.com/typpo/b2b828a35e683b9bf8db91b5404f1bd1,
+# now with Faroese included.
+# Thanks to Ian Webster, https://github.com/typpo
+
+BCP47 = [
+"ar",  #Arabic
+"bn",  #Bangla
+"cs",  #Czech
+"da",  #Danish
+"de",  #Austrian
+"el",  #Greek
+"en",  #English
+"es",  #Spanish
+"fi",  #Finnish
+"fo",  #Faroese
+"fr",  #French
+"he",  #Hebrew
+"hi",  #Hindi
+"hu",  #Hungarian
+"id",  #Indonesian
+"it",  #Italian
+"ja",  #Japanese
+"ko",  #Korean
+"nl",  #Dutch
+"no",  #Norwegian
+"pl",  #Polish
+"pt",  #Portuguese
+"ro",  #Romanian
+"ru",  #Russian
+"sk",  #Slovak
+"sv",  #Swedish
+"ta",  #Tamil
+"th",  #Thai
+"tr",  #Turkish
+"zh",  #Mandarin
+"ar-SA",  #Arabic (Saudi Arabia)
+"bn-BD",  #Bangla (Bangladesh)
+"bn-IN",  #Bangla (India)
+"cs-CZ",  #Czech (Czech Republic)
+"da-DK",  #Danish (Denmark)
+"de-AT",  #Austrian German
+"de-CH",  #"Swiss" German
+"de-DE",  #Standard German (as spoken in Germany)
+"el-GR",  #Modern Greek
+"en-AU",  #Australian English
+"en-CA",  #Canadian English
+"en-GB",  #British English
+"en-IE",  #Irish English
+"en-IN",  #Indian English
+"en-NZ",  #New Zealand English
+"en-US",  #US English
+"en-ZA",  #English (South Africa)
+"es-AR",  #Argentine Spanish
+"es-CL",  #Chilean Spanish
+"es-CO",  #Colombian Spanish
+"es-ES",  #Castilian Spanish (as spoken in Central-Northern Spain)
+"es-MX",  #Mexican Spanish
+"es-US",  #American Spanish
+"fi-FI",  #Finnish (Finland)
+"fo-FO",  #Faroese (Faroe Islands)
+"fr-BE",  #Belgian French
+"fr-CA",  #Canadian French
+"fr-CH",  #"Swiss" French
+"fr-FR",  #Standard French (especially in France)
+"he-IL",  #Hebrew (Israel)
+"hi-IN",  #Hindi (India)
+"hu-HU",  #Hungarian (Hungary)
+"id-ID",  #Indonesian (Indonesia)
+"it-CH",  #"Swiss" Italian
+"it-IT",  #Standard Italian (as spoken in Italy)
+"ja-JP",  #Japanese (Japan)
+"ko-KR",  #Korean (Republic of Korea)
+"nl-BE",  #Belgian Dutch
+"nl-NL",  #Standard Dutch (as spoken in The Netherlands)
+"no-NO",  #Norwegian (Norway)
+"pl-PL",  #Polish (Poland)
+"pt-BR",  #Brazilian Portuguese
+"pt-PT",  #European Portuguese (as written and spoken in Portugal)
+"ro-RO",  #Romanian (Romania)
+"ru-RU",  #Russian (Russian Federation)
+"sk-SK",  #Slovak (Slovakia)
+"sv-SE",  #Swedish (Sweden)
+"ta-IN",  #Indian Tamil
+"ta-LK",  #Sri Lankan Tamil
+"th-TH",  #Thai (Thailand)
+"tr-TR",  #Turkish (Turkey)
+"zh-CN",  #Mainland China, simplified characters
+"zh-HK",  #Hong Kong, traditional characters
+"zh-TW",  #Taiwan, traditional characters
+]
